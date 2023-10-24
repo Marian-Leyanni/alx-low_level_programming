a56dd17bfd1a9fa9fa3ba3ac4a03b222fc3e45c7
Marian-Leyanni <leyanni98@gmail.com>
2023-10-24 14:31:53 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Return: Always (0)
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if (letter != 'e' && letter != 'q')
+		{
+			putchar(letter);
+		}
+		letter++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
